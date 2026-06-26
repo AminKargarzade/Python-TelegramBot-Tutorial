a0e2b290f51adf57df15811e3b1697f5fb9988f1
Amin <aminkargarzadeh26@gmail.com>
2026-06-27 00:28:12 +0330

    Using Logger!

--- core/main.py
+++ core/main.py
@@ -1,31 +1,21 @@
 import telebot
-from telebot import apihelper
 import os
+import logging
 
-apihelper.ENABLE_MIDDLEWARE = True
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)  # Outputs debug messages to console.
 
 API_TOKEN = os.environ.get("API_TOKEN", "")
-
 bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
+    logger.info("Triggered welcome :)")
     bot.reply_to(
         message, """Hi This is a sample for learning telegram bot in python."""
     )
-
-
-@bot.middleware_handler(update_types=["message"])
-def modify_message(bot_instance, message):
-    # modifying the message before it reaches any other handler
-    message.another_text = message.text + ":changed :)"
-
-
-@bot.message_handler(func=lambda message: True)
-def reply_modified(message):
-    bot.reply_to(message, message.another_text)
 
 
 bot.infinity_polling()
