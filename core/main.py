f5862b08e67da5d879381c21247c7b98c1782c46
Amin <aminkargarzadeh26@gmail.com>
2026-06-28 01:09:45 +0330

    Reviewing Keyboard Button!

--- core/main.py
+++ core/main.py
@@ -1,37 +1,40 @@
 import telebot
 import os
 import logging
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
+
 
 API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
 
-@bot.message_handler(commands=["help", "start"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
     logger.info("Triggered welcome :)")
-    bot.reply_to(
-        message, """Hi This is a sample for learning telegram bot in python."""
+    markup = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        input_field_placeholder="Choose Your option: ",
+        one_time_keyboard=True,
+    )
+    markup.add(KeyboardButton("Help"), KeyboardButton("About"))
+    bot.send_message(
+        message.chat.id,
+        """Hi This is a sample for learning telegram bot in python.""",
+        reply_markup=markup,
     )
 
 
-@bot.message_handler(commands=["setname"])
-def setup_name(message):
-    bot.send_message(message.chat.id, "What Is Your First Name?")
-    bot.register_next_step_handler(message, assign_first_name)
+@bot.message_handler(func=lambda message: message.text == "Help")
+def send_help(message):
+    bot.send_message(message.chat.id, "This is the help message.")
 
 
-def assign_first_name(message, *args, **kwargs):
-    first_name = message.text
-    bot.send_message(message.chat.id, "What Is Your Last Name?")
-    bot.register_next_step_handler(message, assign_last_name, first_name)
-
-
-def assign_last_name(message, first_name):
-    last_name = message.text
-    bot.send_message(message.chat.id, f"Welcome to my bot {first_name} {last_name}!")
+@bot.message_handler(func=lambda message: message.text == "About")
+def send_about(message):
+    bot.send_message(message.chat.id, "This is the about message.")
 
 
 bot.infinity_polling()
