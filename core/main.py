f3708e40612a96c191fb46ad3dcaf28604e52b83
Amin <aminkargarzadeh26@gmail.com>
2026-06-28 20:14:13 +0330

    Creating sequence with inline keyboard

--- core/main.py
+++ core/main.py
@@ -16,10 +16,12 @@ def send_welcome(message):
     logger.info("Triggered welcome :)")
     markup = InlineKeyboardMarkup()
     button_google = InlineKeyboardButton("Google", url="https://www.google.com")
-    button_my_github = InlineKeyboardButton("My_Github", url="https://github.com/AminKargarzade")
-    button_test = InlineKeyboardButton("Test", callback_data="test")
+    button_my_github = InlineKeyboardButton(
+        "My_Github", url="https://github.com/AminKargarzade"
+    )
+    button_step1 = InlineKeyboardButton("Step 1", callback_data="step1")
     markup.add(button_google, button_my_github)
-    markup.add(button_test)
+    markup.add(button_step1)
     bot.send_message(
         message.chat.id,
         """Hi This is a sample for learning telegram bot in python.""",
@@ -30,9 +32,32 @@ def send_welcome(message):
 @bot.callback_query_handler(func=lambda call: True)
 def reply_call(call):
     # logger.info(call.__dict__)
-    if call.data == "test":
+    if call.data == "step1":
+        # bot.answer_callback_query(
+        #     call.id, "You clicked the test button!", show_alert=False
+        # )
+        markup = InlineKeyboardMarkup()
+        button_step2 = InlineKeyboardButton("Step 2", callback_data="step2")
+        button_cancel = InlineKeyboardButton("Cancel", callback_data="cancel")
+        markup.add(button_step2, button_cancel)
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.id,
+            text="Choose Your Next Step: ",
+            reply_markup=markup,
+        )
+    if call.data == "step2":
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.id,
+            text="Your Refrence code is: 1234567890",
+        )
+    if call.data == "cancel":
         bot.answer_callback_query(
-            call.id, "You clicked the test button!", show_alert=False
+            call.id, "Process has been canceled!", show_alert=True
+        )
+        bot.delete_message(
+            chat_id=call.message.chat.id, message_id=call.message.id, timeout=5
         )
 
 
