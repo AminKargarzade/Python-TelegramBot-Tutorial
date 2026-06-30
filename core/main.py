8dbe7c51d90f4b0f26cfcaeeceb767442aabfbe2
Amin <aminkargarzadeh26@gmail.com>
2026-07-01 01:05:51 +0330

    Managing and Sending Files!
    In this commit, We wanna send files from local storage of project! (we have 2 other ways:1-file id, 2- file URL)

--- core/main.py
+++ core/main.py
@@ -1,7 +1,6 @@
 import telebot
 import os
 import logging
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -13,52 +12,36 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    logger.info("Triggered welcome :)")
-    markup = InlineKeyboardMarkup()
-    button_google = InlineKeyboardButton("Google", url="https://www.google.com")
-    button_my_github = InlineKeyboardButton(
-        "My_Github", url="https://github.com/AminKargarzade"
-    )
-    button_step1 = InlineKeyboardButton("Step 1", callback_data="step1")
-    markup.add(button_google, button_my_github)
-    markup.add(button_step1)
+    logger.info(message.chat.__dict__)
     bot.send_message(
-        message.chat.id,
-        """Hi This is a sample for learning telegram bot in python.""",
-        reply_markup=markup,
+        message.chat.id, """Hi This is a sample for learning telegram bot in python."""
     )
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def reply_call(call):
-    # logger.info(call.__dict__)
-    if call.data == "step1":
-        # bot.answer_callback_query(
-        #     call.id, "You clicked the test button!", show_alert=False
-        # )
-        markup = InlineKeyboardMarkup()
-        button_step2 = InlineKeyboardButton("Step 2", callback_data="step2")
-        button_cancel = InlineKeyboardButton("Cancel", callback_data="cancel")
-        markup.add(button_step2, button_cancel)
-        bot.edit_message_text(
-            chat_id=call.message.chat.id,
-            message_id=call.message.id,
-            text="Choose Your Next Step: ",
-            reply_markup=markup,
-        )
-    if call.data == "step2":
-        bot.edit_message_text(
-            chat_id=call.message.chat.id,
-            message_id=call.message.id,
-            text="Your Refrence code is: 1234567890",
-        )
-    if call.data == "cancel":
-        bot.answer_callback_query(
-            call.id, "Process has been canceled!", show_alert=True
-        )
-        bot.delete_message(
-            chat_id=call.message.chat.id, message_id=call.message.id, timeout=5
-        )
+@bot.message_handler(commands=["test_voice"])
+def send_voice_file(message):
+    voice_file = open("./test/files/test.mp3", "rb")
+    bot.send_chat_action(message.chat.id, action="upload_voice")
+    bot.send_voice(message.chat.id, voice_file)
+
+
+@bot.message_handler(commands=["test_video"])
+def send_video_file(message):
+    bot.send_chat_action(message.chat.id, action="upload_video")
+    bot.send_video(message.chat.id, open("./test/files/test.mp4", "rb"))
+
+
+@bot.message_handler(commands=["test_document"])
+def send_document_file(message):
+    bot.send_chat_action(message.chat.id, action="upload_document")
+    bot.send_document(message.chat.id, open("./test/files/test.pdf", "rb"))
+
+
+@bot.message_handler(commands=["test_photo"])
+def send_photo_file(message):
+    bot.send_chat_action(message.chat.id, action="upload_photo")
+    bot.send_document(message.chat.id, open("./test/files/test.jpg", "rb"))
+    bot.send_photo(message.chat.id, open("./test/files/test.jpg", "rb"))
 
 
 bot.infinity_polling()
