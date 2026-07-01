c28871a4869250323a771bfe5908e6d8c5428c20
Amin <aminkargarzadeh26@gmail.com>
2026-07-01 19:09:05 +0330

    Customizing the bot from BotFather!

--- core/main.py
+++ core/main.py
@@ -13,31 +13,21 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    logger.info(message.chat.__dict__)
     bot.send_message(
         message.chat.id, """Hi This is a sample for learning telegram bot in python."""
     )
 
 
-@bot.message_handler(commands=["upload_test"])
-def send_document_file(message):
+@bot.message_handler(commands=["author"])
+def send_author(message):
     markup = InlineKeyboardMarkup()
-    like_btn = InlineKeyboardButton("Like", callback_data="like")
-    dislike_btn = InlineKeyboardButton("Dislike", callback_data="dislike")
-    markup.add(like_btn, dislike_btn)
-    bot.send_video(
-        message.chat.id,
-        "BAACAgQAAxkBAAIBMmpFDnKCyt4hykEgA1aCTgfDsXZzAAKCHwACR3MpUgZnsUqOtLtpPAQ",
-        caption="This is a sample video file",
-        reply_markup=markup,
+    myGithubButton = InlineKeyboardButton(
+        "My Github", url="https://github.com/AminKargarzade"
     )
-
-
-@bot.message_handler(
-    content_types=["document", "video_note", "video", "audio", "voice", "photo"]
-)
-def check_id(message):
-    logger.info(message.__dict__)
+    markup.add(myGithubButton)
+    bot.send_message(
+        message.chat.id, "This bot was created by AminRastin !", reply_markup=markup
+    )
 
 
 bot.infinity_polling()
