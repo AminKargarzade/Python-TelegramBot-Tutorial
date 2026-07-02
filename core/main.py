e3a35c3bbb3d33016bb1e985d45e7df1006a128b
Amin <aminkargarzadeh26@gmail.com>
2026-07-02 17:36:30 +0330

    Reviewing Inline Query!

--- core/main.py
+++ core/main.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 import logging
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import InlineQueryResultArticle, InputTextMessageContent
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -18,16 +18,42 @@ def send_welcome(message):
     )
 
 
-@bot.message_handler(commands=["author"])
-def send_author(message):
-    markup = InlineKeyboardMarkup()
-    myGithubButton = InlineKeyboardButton(
-        "My Github", url="https://github.com/AminKargarzade"
+@bot.inline_handler(func=lambda query: len(query.query) >= 0)
+def query_handler(query):
+    logger.info(query)
+    results = []
+
+    results.append(
+        InlineQueryResultArticle(
+            id="1",
+            title="This is a test",
+            input_message_content=InputTextMessageContent(
+                message_text="This is a response"
+            ),
+            description="This is a description ",
+        )
     )
-    markup.add(myGithubButton)
-    bot.send_message(
-        message.chat.id, "This bot was created by AminRastin !", reply_markup=markup
+    results.append(
+        InlineQueryResultArticle(
+            id="2",
+            title="Join the bot",
+            input_message_content=InputTextMessageContent(message_text="JOIN the bot"),
+            url="https://t.me/py_rastin_bot",
+        )
     )
+
+    results.append(
+        InlineQueryResultArticle(
+            id="3",
+            title="Check out my Github",
+            input_message_content=InputTextMessageContent(
+                message_text="https://github.com/AminKargarzade"
+            ),
+            url="https://github.com/AminKargarzade",
+        )
+    )
+
+    bot.answer_inline_query(query.id, results, cache_time=0)
 
 
 bot.infinity_polling()
