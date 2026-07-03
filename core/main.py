5af3017b9137c8fd1696810306b241fa9e0980a4
Amin <aminkargarzadeh26@gmail.com>
2026-07-03 17:26:47 +0330

    sample File Downloader Project!
    it just downloads documents!

--- core/main.py
+++ core/main.py
@@ -1,60 +1,53 @@
 import telebot
 import os
 import logging
+import requests
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
+if not os.path.exists("downloads"):
+    os.makedirs("downloads")
+
 API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
-# MarkDown
-# *bold text*
-# _italic text_
-# `inline code`
-# [inline URL](https://github.com/AminKargarzade)
-# [mention](tg://user?id=USER_ID)
-
-# HTML
-# <b>bold text</b>
-# <i>italic text</i>
-# <code>inline code</code>
-# <a href="https://github.com/AminKargarzade">inline URL</a>
-# <a href="tg://user?id=USER_ID">mention</a>
+DOWNLOAD_DIR = "downloads/"
 
 
-@bot.message_handler(commands=["markdown"])
-def handle_messages(message):
-    bot.send_message(message.chat.id, "*bold text*", parse_mode="Markdown")
-    bot.send_message(message.chat.id, "_italic text_", parse_mode="Markdown")
-    bot.send_message(message.chat.id, "`import requests`", parse_mode="Markdown")
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
     bot.send_message(
         message.chat.id,
-        "[inline URL](https://github.com/AminKargarzade)",
-        parse_mode="Markdown",
-    )
-    bot.send_message(
-        message.chat.id,
-        f"[mention](tg://user?id={message.from_user.id})",
-        parse_mode="Markdown",
+        "Give me a valid URL for a file and I will download it for you.",
     )
 
 
-@bot.message_handler(commands=["html"])
-def handle_messages2(message):
-    bot.send_message(message.chat.id, "<b>bold text</b>", parse_mode="HTML")
-    bot.send_message(message.chat.id, "<i>italic text</i>", parse_mode="HTML")
-    bot.send_message(message.chat.id, "<code>inline code</code>", parse_mode="HTML")
-    bot.send_message(
-        message.chat.id,
-        '<a href="https://github.com/AminKargarzade">inline URL</a>',
-        parse_mode="HTML",
-    )
-    bot.send_message(
-        message.chat.id,
-        f'<a href="tg://user?id={message.from_user.id}">mention</a>',
-        parse_mode="HTML",
-    )
+def download_file(url):
+    local_filename = url.split("/")[-1]
+    file_path = DOWNLOAD_DIR + local_filename
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(file_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return file_path
+
+
+@bot.message_handler(func=lambda message: True)
+def download_file_url(message):
+    logger.info(message.text)
+    url = message.text
+    try:
+        file_path = download_file(url)
+        bot.send_document(chat_id=message.chat.id, reply_parameters=telebot.types.ReplyParameters(message.id), document=open(file_path, "rb"), caption="File downloaded successfully.")  # type: ignore
+        # os.remove(file_path)  # Clean up the downloaded file after sending
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error occurred while downloading file from {url}: {e}")
+        bot.reply_to(
+            message,
+            text="Failed to download the file. Please check the URL and try again.",
+        )
 
 
 bot.infinity_polling()
