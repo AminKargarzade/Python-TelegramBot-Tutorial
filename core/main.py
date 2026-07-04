fce0f664abe10dfafd021eafcee8bf19d5bca3df
Amin <aminkargarzadeh26@gmail.com>
2026-07-04 23:47:30 +0330

    Managing Messages in Group
    We have different types of messages in Group Chat & Private Chat with Bot!

--- core/main.py
+++ core/main.py
@@ -1,7 +1,6 @@
 import telebot
 import os
 import logging
-import requests
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -11,34 +10,14 @@ API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
 
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-    bot.send_message(
-        message.chat.id,
-        "لطفا کلید واژه مورد نظر خود را برای سرچ در دیوار را وارد کنید!",
-    )
+@bot.message_handler(func=lambda message: message.chat.type in ["group", "supergroup"])
+def handle_group_message(message):
+    logger.info("Group Triggered!")
 
 
-def fetch_divar_ads(text_query):
-    url = "https://api.divar.ir/v8/web-search/urmia"
-    params = {"q": text_query}
-    response = requests.get(url=url, params=params)
-    return response.json()
-
-
-@bot.message_handler(func=lambda message: True)
-def fetch_ads(message):
-    text = message.text
-    data = fetch_divar_ads(text)
-    for item in data["web_widgets"]["post_list"][1:11]:
-        title = item["data"]["title"]
-        top_description_text = item["data"]["top_description_text"]
-        middle_description_text = item["data"]["middle_description_text"]
-        bottom_description_text = item["data"]["bottom_description_text"]
-        photo = item["data"]["image_url"][0]["src"]
-        token = item["data"]["token"]
-        description = f"{title}\n{top_description_text}\n{middle_description_text}\n{bottom_description_text}\nhttps://divar.ir/v/{token}"
-        bot.send_photo(chat_id=message.chat.id, caption=description, photo=photo)
+@bot.message_handler(func=lambda message: message.chat.type in ["private"])
+def handle_private_message(message):
+    logger.info("Private Triggered!")
 
 
 bot.infinity_polling()
