064fb59c45bf1fd46467923d555421996b9ea7d0
Amin <aminkargarzadeh26@gmail.com>
2026-07-07 00:59:06 +0330

    Night-Mode project

--- core/main.py
+++ core/main.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 import logging
+from telebot.types import ChatPermissions
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -8,36 +9,63 @@ telebot.logger.setLevel(logging.INFO)
 API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
-# get_chat_administrators
-# get_chat_member
-# get_chat_members_count
-# get_chat
 
-# @bot.message_handler(func=lambda message: True)
-# def handle_messages(message):
-#     data = bot.get_chat(message.chat.id)
-#     logger.info(data)
+def is_admin(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
 
-# @bot.message_handler(func=lambda message: True)
-# def handle_messages(message):
-#     data = bot.get_chat_member_count(message.chat.id)
-#     logger.info(data)
-
-# @bot.message_handler(func=lambda message: True)
-# def handle_messages(message):
-#     data = bot.get_chat_member(message.chat.id, message.from_user.id)
-#     logger.info(data)
-
-# @bot.message_handler(func=lambda message: True)
-# def handle_messages(message):
-#     data = bot.get_chat_administrators(message.chat.id)
-#     for user in data:
-#         logger.info(user)
+    user_info = bot.get_chat_member(chat_id, user_id)
+    return user_info.status in ["administrator", "creator"]
 
 
-@bot.message_handler(commands=["ban"])
-def handle_messages(message):
-    logger.info(message.text.split(" ")[1])
+@bot.message_handler(func=is_admin, commands=["restrict"])
+def handle_restriction(message):
+    permissions = ChatPermissions(
+        can_send_messages=False,
+        can_send_media_messages=False,
+        can_send_audios=False,
+        can_send_documents=False,
+        can_send_photos=False,
+        can_send_videos=False,
+        can_send_video_notes=False,
+        can_send_voice_notes=False,
+        can_send_polls=False,
+        can_send_other_messages=False,
+        can_add_web_page_previews=False,
+        can_react_to_messages=False,
+    )
+    bot.set_chat_permissions(message.chat.id, permissions=permissions)
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_message(
+        message.chat.id,
+        "Night Mode Activated! All members are restricted from sending messages,go sleep now!",
+    )
+    logger.info("Applying Restriction")
+
+
+@bot.message_handler(func=is_admin, commands=["unrestrict"])
+def handle_unrestriction(message):
+    permissions = ChatPermissions(
+        can_send_messages=True,
+        can_send_media_messages=True,
+        can_send_audios=True,
+        can_send_documents=True,
+        can_send_photos=True,
+        can_send_videos=True,
+        can_send_video_notes=True,
+        can_send_voice_notes=True,
+        can_send_polls=True,
+        can_send_other_messages=True,
+        can_add_web_page_previews=True,
+        can_react_to_messages=True,
+    )
+    bot.set_chat_permissions(message.chat.id, permissions=permissions)
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_message(
+        message.chat.id,
+        "Day Mode Activated! All members are now allowed to send messages.Talk now!",
+    )
+    logger.info("Removing Restriction")
 
 
 bot.infinity_polling()
