4bc922997c9cc73c4e0caeef803ab350a85abd4d
Amin <aminkargarzadeh26@gmail.com>
2026-07-08 22:41:46 +0330

    Connecting the bot to the Channel!

--- core/main.py
+++ core/main.py
@@ -8,40 +8,8 @@ telebot.logger.setLevel(logging.INFO)
 API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
-bad_words_list = [
-    "badword1",
-    "badword2",
-    "badword3",
-]  # Replace with your list of bad words
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 
+bot.send_message(chat_id=CHANNEL_ID, text="Hello from the bot!")
 
-warnings = {}
-
-
-def has_bad_words(text):
-    return any(bad_word in text for bad_word in bad_words_list)
-
-
-@bot.message_handler(func=lambda message: True)
-def message_handler(message):
-
-    if has_bad_words(message.text):
-        if str(message.from_user.id) in warnings:
-            warnings[str(message.from_user.id)] += 1
-        else:
-            warnings[str(message.from_user.id)] = 1
-
-        if warnings[str(message.from_user.id)] > 3:
-            bot.kick_chat_member(message.chat.id, message.from_user.id)
-            bot.send_message(
-                message.chat.id,
-                f"{message.from_user.first_name} has been kicked out for using bad words.",
-            )
-        else:
-            bot.reply_to(
-                message,
-                f"Don't use bad words! This is your {warnings[str(message.from_user.id)]} warnings, after 3rd warning you will be kicked out.",
-            )
-
-
-bot.infinity_polling()
+# bot.infinity_polling() We don't need this anymore while interacting with Channels, because we're only sending messages and not listening for incoming messages!
