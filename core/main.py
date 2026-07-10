daf4bef05baa85765a9fdd1159f1c7e6a2ef2d90
Amin <aminkargarzadeh26@gmail.com>
2026-07-11 00:11:29 +0330

    Analyzing the performance of a bot!

--- core/main.py
+++ core/main.py
@@ -9,24 +9,11 @@ API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
 CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
-CHANNEL_LINK = "https://t.me/pyrastinchannel"
 
 
-def is_member(message):
-    user_info = bot.get_chat_member(CHANNEL_ID, message.from_user.id)
-    if user_info.status not in ["member", "administrator", "creator"]:
-        bot.send_message(
-            message.chat.id,
-            f"Please Subscribe to the channel first: [Join Channel]({CHANNEL_LINK}) ",
-            parse_mode="Markdown",
-        )
-        return False
-    return True
-
-
-@bot.message_handler(func=is_member)
+@bot.message_handler(commands=["start"])
 def message_handler(message):
-    bot.reply_to(message, "ok")
+    logger.info(message.text.split()[1])
 
 
 bot.infinity_polling()
