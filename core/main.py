74a2f72a789025cb1c83b912f9d51eb7bed4aa18
Amin <aminkargarzadeh26@gmail.com>
2026-07-10 20:53:28 +0330

    Project First Join Then Use it!

--- core/main.py
+++ core/main.py
@@ -9,7 +9,24 @@ API_TOKEN = os.environ.get("API_TOKEN", "")
 bot = telebot.TeleBot(API_TOKEN)
 
 CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
+CHANNEL_LINK = "https://t.me/pyrastinchannel"
 
-bot.send_message(chat_id=CHANNEL_ID, text="Hello from the bot!")
 
-# bot.infinity_polling() We don't need this anymore while interacting with Channels, because we're only sending messages and not listening for incoming messages!
+def is_member(message):
+    user_info = bot.get_chat_member(CHANNEL_ID, message.from_user.id)
+    if user_info.status not in ["member", "administrator", "creator"]:
+        bot.send_message(
+            message.chat.id,
+            f"Please Subscribe to the channel first: [Join Channel]({CHANNEL_LINK}) ",
+            parse_mode="Markdown",
+        )
+        return False
+    return True
+
+
+@bot.message_handler(func=is_member)
+def message_handler(message):
+    bot.reply_to(message, "ok")
+
+
+bot.infinity_polling()
